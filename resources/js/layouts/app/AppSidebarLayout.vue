0592b92c8105394cc49d0c451564acce143a62b3
owenbcoding <eoghankb@gmail.com>
2025-08-01 04:06:04 +0100

    created and modified sidebar layout

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import AppContent from '@/components/AppContent.vue';
-import AppShell from '@/components/AppShell.vue';
-import AppSidebar from '@/components/AppSidebar.vue';
-import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
+import NavMain from '@/components/NavMain.vue';
 import type { BreadcrumbItemType } from '@/types';
 
 interface Props {
@@ -15,11 +13,10 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <AppShell variant="sidebar">
-        <AppSidebar />
-        <AppContent variant="sidebar" class="overflow-x-hidden">
-            <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+    <div>
+        <NavMain :items="[]" />
+        <AppContent class="overflow-x-hidden">
             <slot />
         </AppContent>
-    </AppShell>
+    </div>
 </template>
