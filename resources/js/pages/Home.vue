f554b42a7fceb5789a5aed9632445811a4426a4b
owenbcoding <eoghankb@gmail.com>
2025-08-01 10:15:59 +0100

    added lang="ts" to script at top

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 import MainNavigation from '@/components/MainNavigation.vue';
 import SliderNavigation from '@/components/SliderNavigation.vue';
